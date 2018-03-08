78c3403984b5d8caecfb2f27f75765e5ef338290
caseyfitz <caseyfitz@users.noreply.github.com>
2018-03-08 15:03:35 -0800

    Parse requirements.txt for install_requires

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 
 setup(
     name='ChimpsTool',
     version='0.0',
     # py_modules=['cmd'],
-    install_requires=[
-        'click',
-        'ffmpy',
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'sphinx',
-        'tensorflow',
-        'matplotlib'
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'cmd=src.cli:main',
