8910512db13235b4304b88e4dd9c23b6980bacbc
Peter Bull <pjbull@gmail.com>
2018-03-15 15:17:48 -0600

    Create model skeleton

--- src/src/models/winning_model.py
+++ src/src/models/winning_model.py
@@ -1,6 +1,6 @@
 from pathlib import Path
+from shutil import rmtree
 import tempfile
-
 
 import pandas as pd
 import tensorflow as tf
@@ -9,5 +9,25 @@ from .model import Model
 
 
 class WinningModel(Model):
-    def __init__(self, prop1=None):
-        self.prop1 = prop1
+    def __init__(self, modeldir, tempdir=None):
+        # use the model object's defaults
+        super().__init__(modeldir, tempdir=tempdir)
+
+    def predict_proba(self, X):
+        """ Predict class probabilities for each input, X
+        """
+        pass
+
+    def fit(self, X, y):
+        """ Use the same architecture, but train the weights from scratch using
+            the provided X and y.
+        """
+        pass
+
+    def finetune(self, X, y):
+        """ Finetune the network for a different task by keeping the
+            trained weights, replacing the top layer with one that outputs
+            the new classes, and re-training for a few epochs to have the
+            model output the new classes instead.
+        """
+        pass
