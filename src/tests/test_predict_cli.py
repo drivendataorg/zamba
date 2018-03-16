98f94778e6e820773a180d63572dea1d6c2c3014
caseyfitz <caseyfitz@users.noreply.github.com>
2018-03-16 11:12:02 -0600

    Trim unneccessary comments and imports

--- src/tests/test_predict_cli.py
+++ src/tests/test_predict_cli.py
@@ -1,27 +1,9 @@
-from pathlib import Path
-import click
-from click.testing import CliRunner
-
-import pandas as pd
-
-from src.cli import predict
-
-# from src.tests.test_model_config import *
-
 
 def test_predict_modelpath(model_subdir):
     """
     This needs work.
-    Saving the below for refrence later,
-    in case we deicide to test cli directly.
+    Saving the below for reference later,
+    in case we decide to test cli directly.
     """
-
-    # runner = CliRunner()
-    # modelpath = str(model_subdir.resolve())
-    # assert type(modelpath) == str
-    # modelpath = str(Path('..', 'models', 'assets').resolve())
-    # result = runner.invoke(predict, ['--modelpath', modelpath])
-    # assert result.exit_code == 0
-
     pass
 
