7746726f826d8c6b866dffbf3c213cc28cf58dbb
caseyfitz <caseyfitz@users.noreply.github.com>
2018-03-23 11:06:22 -0700

    Update manager docs

--- djamba/models/manager.py
+++ djamba/models/manager.py
@@ -6,6 +6,9 @@ from djamba.models.winning_model import WinningModel
 
 
 class GetItemMeta(EnumMeta):
+    """Complicated override so that we can use hashing in ModelManager init.
+
+    """
     def __getitem__(cls, name):
         for e in cls:
             if e.string == name:
@@ -15,6 +18,14 @@ class GetItemMeta(EnumMeta):
 
 
 class ModelName(Enum, metaclass=GetItemMeta):
+    """Allows easy control over which Model subclass to load, sample or winning.
+
+        Args:
+            string (str) : string for switching between test and default model
+            model (Model) : Model subclass to instantiate into manager.
+
+    """
+
     WINNING = ('winning', WinningModel)
     SAMPLE = ('sample', SampleModel)
 
@@ -27,10 +38,23 @@ class ModelName(Enum, metaclass=GetItemMeta):
 
 
 class ModelManager(object):
-    """
-    Class to mediate loading,
-    configuration, and logic
-    of model calls.
+    """Mediates loading, configuration, and logic of model calls.
+
+        Args:
+            model_path (str | Path) : path to model
+                Required argument (for now)
+            data_path (str | Path) : path to data
+                Defaults to ``None``.
+            pred_path (str | Path) : where predictions will be saved
+                Defaults to ``None``.
+            proba_threshold (float) : threshold for classification
+                Defaults to ``None``.
+            tempdir (str | Path) : path to temporary directory
+                Defaults to ``None``.
+            verbose (bool) : controls verbosity
+                Defaults to ``True``.
+            model_class (str) : controls whether sample class or production is used
+                Defaults to "winning". Must be "winning" or "sample".
     """
     def __init__(self,
                  model_path,
@@ -54,7 +78,13 @@ class ModelManager(object):
 
     def predict(self, data_path=None, pred_path=None):
         """
-        Handle prediction
+
+        Args:
+            data_path (str | Path) : path to data, checks model_path attr if ``None`
+            pred_path (str | Path) : where predictions will be saved
+
+        Returns: DataFrame of predictions
+
         """
 
         if data_path is None:
@@ -75,7 +105,17 @@ class ModelManager(object):
             return preds >= self.proba_threshold
 
     def train(self):
+        """
+
+        Returns:
+
+        """
         pass
 
     def tune(self):
+        """
+
+        Returns:
+
+        """
         pass
