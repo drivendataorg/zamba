15977d8c7f855a73dde31eabc76588d2b6396c15
Peter Bull <pjbull@gmail.com>
2018-03-23 23:34:35 -0700

    Add skipped test for cnnensemble

--- zamba/tests/test_cnnensemble.py
+++ zamba/tests/test_cnnensemble.py
@@ -1,7 +1,9 @@
+import pytest
+
 from zamba.models.manager import ModelManager
 
 
-def test_predict():
+@pytest.mark.xfail(reason="Not yet implemented!")
+def test_predict(mocker):
     manager = ModelManager('', model_class='cnnensemble')
-
     manager.predict('')
