0d4c6a73742cd2236dab2547046b7294e95cb1db
caseyfitz <caseyfitz@users.noreply.github.com>
2018-04-02 15:30:37 -0700

    Use overridden slash op after Isaac review

--- zamba/models/cnnensemble/src/config.py
+++ zamba/models/cnnensemble/src/config.py
@@ -4,9 +4,9 @@ cnnensemble_dir = Path(__file__).parent.parent
 RAW_VIDEO_DIR = cnnensemble_dir / "input" / "raw"
 TRAIN_IMG_DIR = cnnensemble_dir / "data_fast" / "pri_matrix" / "train_img"
 TEST_VIDEO_DIR = cnnensemble_dir / "input" / "raw_test"
-UNUSED_VIDEO_DIR = cnnensemble_dir / 'input/raw_unused/'
+UNUSED_VIDEO_DIR = cnnensemble_dir / "input" / "raw_unused"
 UNUSED_IMG_DIR = cnnensemble_dir / "input" / "raw_unused_img"
-SMALL_VIDEO_DIR = cnnensemble_dir / 'input/small/'
+SMALL_VIDEO_DIR = cnnensemble_dir / "input" / "small"
 TRAINING_SET_LABELS = cnnensemble_dir / "input" / "Pri-matrix_Factorization_-_Training_Set_Labels.csv"
 SUBMISSION_FORMAT = cnnensemble_dir / "input" / "Pri-matrix_Factorization_-_Submission_Format.csv"
 
