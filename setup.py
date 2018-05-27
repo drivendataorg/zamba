eebe5bcc1cbcfcc0631b7ffa6b6ae08ae393e374
Peter Bull <peter@drivendata.org>
2018-05-27 14:27:00 -0700

    rev version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = open(Path(__file__).parent / 'README.md').read()
 
 setup(
     name='zamba',
-    version='0.1.4',
+    version='0.1.5',
     description='Zamba is a tool to identify the species seen in camera trap videos from sites in central Africa.',
     long_description=long_description,
     long_description_content_type='text/markdown',
