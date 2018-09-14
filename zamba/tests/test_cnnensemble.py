c48e82f04329613ec16556856d2fcb166b16092e
caseyfitz <caseyfitz@users.noreply.github.com>
2018-09-14 14:16:27 -0700

    Replace cnnensemble hardcoded paths prefixes with config.MODEL_DIR

--- zamba/tests/test_cnnensemble.py
+++ zamba/tests/test_cnnensemble.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import pytest
+from zamba.models.cnnensemble.src import config
 
 from zamba.models.manager import ModelManager
 
@@ -7,7 +7,7 @@ from zamba.models.manager import ModelManager
 @pytest.mark.skip(reason="This test takes hours to run, makes network calls, and is really for local dev only.")
 def test_predict():
 
-    data_dir = Path(__file__).parent.parent / "models" / "cnnensemble" / "input" / "raw_test"
+    data_dir = config.MODEL_DIR / "input" / "raw_test"
 
     manager = ModelManager('', model_class='cnnensemble', proba_threshold=0.5)
     manager.predict(data_dir, save=True)
