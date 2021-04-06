bd9cbb374bc7246f42127f606f6959d3ba638df3
Emily Miller <ejm714@gmail.com>
2021-04-06 15:13:18 -0700

    black formatting

--- zamba/new_cli.py
+++ zamba/new_cli.py
@@ -8,25 +8,47 @@ from zamba.models.manager import TrainConfig, PredictConfig, ModelManager
 
 app = typer.Typer()
 
+
 @app.command()
 def train(
-    train_data: Path = typer.Option(Path("train_videos"), help="Path to folder containing training videos."),
-    val_data: Path = typer.Option(Path("val_videos"), help="Path to folder containing validation videos."),
+    train_data: Path = typer.Option(
+        Path("train_videos"), help="Path to folder containing training videos."
+    ),
+    val_data: Path = typer.Option(
+        Path("val_videos"), help="Path to folder containing validation videos."
+    ),
     labels: Path = typer.Option(Path("labels.csv"), help="Path to csv containing video labels."),
-    config: Path = typer.Option(None, help="Specify options using yaml configuration file instead of through command line options."),
+    config: Path = typer.Option(
+        None,
+        help="Specify options using yaml configuration file instead of through command line options.",
+    ),
     model_path: Path = typer.Option(Path("custom_model.h5"), help="Path to model to train."),
     model_library: str = typer.Option("keras", help="Library to use for loading custom model."),
     n_epochs: Optional[int] = typer.Option(10, help="Number of epochs to train."),
-    tempdir: Optional[Path] = typer.Option(None, help="Path to temporary directory. If not specified, OS temporary directory is used"),
-    model_class: str = typer.Option("custom", help="[Not implemented] Model from model zoo to train. Currently only external custom models are supported."),
-    height: Optional[int] = typer.Option(None, help="[Not implemented] Desired height for resized video."),
-    width: Optional[int] = typer.Option(None, help="[Not implemented] Desired width for resized video."),
-    augmentation: Optional[bool] = typer.Option(False, help="[Not implemented] If True, flip and rotate videos during training."),
-    early_stopping: Optional[bool] = typer.Option(False, help="[Not implemented] If True, use early stopping."),
-    save_path: Optional[Path] = typer.Option(None, help="[Not implemented] Save out trained model to this filepath.")
+    tempdir: Optional[Path] = typer.Option(
+        None, help="Path to temporary directory. If not specified, OS temporary directory is used"
+    ),
+    model_class: str = typer.Option(
+        "custom",
+        help="[Not implemented] Model from model zoo to train. Currently only external custom models are supported.",
+    ),
+    height: Optional[int] = typer.Option(
+        None, help="[Not implemented] Desired height for resized video."
+    ),
+    width: Optional[int] = typer.Option(
+        None, help="[Not implemented] Desired width for resized video."
+    ),
+    augmentation: Optional[bool] = typer.Option(
+        False, help="[Not implemented] If True, flip and rotate videos during training."
+    ),
+    early_stopping: Optional[bool] = typer.Option(
+        False, help="[Not implemented] If True, use early stopping."
+    ),
+    save_path: Optional[Path] = typer.Option(
+        None, help="[Not implemented] Save out trained model to this filepath."
+    ),
 ):
-    """Train a custom model using the provided data, labels, and path to preconfigured model architecture.
-    """
+    """Train a custom model using the provided data, labels, and path to preconfigured model architecture."""
     if config is not None:
         manager = ModelManager.from_config(config)
 
@@ -61,18 +83,24 @@ def train(
 def predict(
     data_path: Path = typer.Option(Path("."), help="Path to videos."),
     model_path: Path = Path("."),
-    model_class: str = 'cnnensemble',
+    model_class: str = "cnnensemble",
     pred_path: Optional[Path] = None,
-    output_class_names: Optional[bool] = typer.Option(False, help="If True, we just return a video and the name of the most likely class. If False, we return a probability or indicator (depending on --proba_threshold) for every possible class."),
+    output_class_names: Optional[bool] = typer.Option(
+        False,
+        help="If True, we just return a video and the name of the most likely class. If False, we return a probability or indicator (depending on --proba_threshold) for every possible class.",
+    ),
     proba_threshold: Optional[float] = None,
     tempdir: Optional[Path] = None,
-    verbose: Optional[bool] = typer.Option(False, help="Displays additional logging information during processing."),
+    verbose: Optional[bool] = typer.Option(
+        False, help="Displays additional logging information during processing."
+    ),
     save: Optional[bool] = False,
     # model_kwargs: Optional[dict] = dict(),
     # predict_kwargs: Optional[dict] = dict(),
 ):
     # TODO: add profile and other params to predict and pass in accordingly
     print(PredictConfig(**kwargs))
+
 
 if __name__ == "__main__":
     app()
