9092a1bb32604737eaecf6ef4a9d281d446428d4
Emily Miller <ejm714@gmail.com>
2021-04-09 14:01:46 -0700

    add tests for train cli; add data-path opt for predict; data-path can no longer be an argument if we support only passing in a config

--- zamba/tests/test_cli.py
+++ zamba/tests/test_cli.py
@@ -1,29 +1,66 @@
+from pathlib import Path
+
 from typer.testing import CliRunner
 from pytest_mock import mocker  # noqa: F401
 
 from zamba.cli import app
 
+
+assets_dir = Path(__file__).parent / 'assets'
 runner = CliRunner()
 
 
-def test_predict_modelpath(sample_model_path, sample_data_path, mocker):  # noqa: F811
+def test_predict_options(sample_model_path, sample_data_path, mocker):  # noqa: F811
     # mock predictions to just test CLI args
     def pred_mock(self):
         return None
 
     mocker.patch('zamba.cli.ModelManager.predict', pred_mock)
 
-    # test the predict cli function
-    result = runner.invoke(app, ['predict', str(sample_data_path)])
+    result = runner.invoke(app, ['predict', '--data-path', str(sample_data_path)])
     assert result.exit_code == 0
 
-    result = runner.invoke(app, ['predict', str(sample_data_path), '--verbose'])
+    result = runner.invoke(app, ['predict', '--data-path', str(sample_data_path), '--verbose'])
     assert result.exit_code == 0
 
-    result = runner.invoke(app, ['predict', str(sample_data_path), '--bad-arg'])
+    result = runner.invoke(app, ['predict', '--data-path', str(sample_data_path), '--bad-arg'])
     assert result.exit_code == 2
     assert 'no such option' in result.output
 
-    result = runner.invoke(app, ['predict', str('not/a/path'), '--verbose'])
+    result = runner.invoke(app, ['predict', '--data-path', str('not/a/path'), '--verbose'])
     assert result.exit_code == 2
     assert "Path 'not/a/path' does not exist" in result.output
+
+    result = runner.invoke(app, ['predict', '--config', str(assets_dir / 'sample_config.yaml')])
+    assert result.exit_code == 0
+
+
+def test_train_options(data_dir, sample_model_path, mocker):  # noqa: F811
+
+    # before mocking ModelManager.train, check non-custom model errors
+    result = runner.invoke(app, ['train', '--model-class', 'cnnensemble'])
+    assert result.exit_code == 1
+    assert "NotImplementedError('Currently only custom models can be trained.'" in str(result)
+
+    # mock training to just test CLI args
+    def train_mock(self):
+        return None
+
+    mocker.patch('zamba.cli.ModelManager.train', train_mock)
+
+    result = runner.invoke(app, ['train', '--train-data', str(data_dir)])
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ['train', '--model-path', str(sample_model_path)])
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ['train', str(data_dir), '--download_region'])
+    assert result.exit_code == 2
+    assert 'no such option' in result.output
+
+    result = runner.invoke(app, ['train', '--model-library', 'fastai'])
+    assert result.exit_code == 2
+    assert "invalid choice: fastai. (choose from keras, pytorch)" in result.output
+
+    result = runner.invoke(app, ['train', '--config', str(assets_dir / 'sample_config.yaml')])
+    assert result.exit_code == 0
