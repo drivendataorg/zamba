670bfe911fb3493ff9194d4a70f38fcae510dcac
Emily Miller <ejm714@gmail.com>
2021-04-09 16:44:38 -0700

    predict on real video using yaml

--- zamba/tests/test_cli.py
+++ zamba/tests/test_cli.py
@@ -11,6 +11,10 @@ runner = CliRunner()
 
 
 def test_predict_options(sample_model_path, sample_data_path, mocker):  # noqa: F811
+    # before mocking predictions, test real prediction on single video using config file
+    result = runner.invoke(app, ['predict', '--config', str(assets_dir / 'sample_config.yaml')])
+    assert result.exit_code == 0
+
     # mock predictions to just test CLI args
     def pred_mock(self):
         return None
@@ -30,9 +34,6 @@ def test_predict_options(sample_model_path, sample_data_path, mocker):  # noqa: 
     result = runner.invoke(app, ['predict', '--data-path', str('not/a/path'), '--verbose'])
     assert result.exit_code == 2
     assert "Path 'not/a/path' does not exist" in result.output
-
-    result = runner.invoke(app, ['predict', '--config', str(assets_dir / 'sample_config.yaml')])
-    assert result.exit_code == 0
 
 
 def test_train_options(data_dir, sample_model_path, mocker):  # noqa: F811
