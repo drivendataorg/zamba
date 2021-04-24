5d493c6d234aabe9502ea82560d1733975222791
Emily Miller <ejm714@gmail.com>
2021-04-23 17:48:24 -0700

    update tests to pass with new config

--- zamba/tests/test_cli.py
+++ zamba/tests/test_cli.py
@@ -12,7 +12,7 @@ runner = CliRunner()
 
 def test_predict_options(sample_data_path, mocker):  # noqa: F811
     # before mocking predictions, test real prediction on single video using config file
-    result = runner.invoke(app, ['predict', '--config', str(assets_dir / 'sample_config.yaml')])
+    result = runner.invoke(app, ['predict', '--config', str(assets_dir / 'sample_predict_config.yaml')])
     assert result.exit_code == 0
 
     # mock predictions to just test CLI args
@@ -36,7 +36,7 @@ def test_predict_options(sample_data_path, mocker):  # noqa: F811
     assert "Path 'not/a/path' does not exist" in result.output
 
 
-def test_train_options(data_dir, mocker):  # noqa: F811
+def test_train_options(data_dir, sample_model_path, mocker):  # noqa: F811
 
     # before mocking ModelManager.train, check non-custom model errors
     result = runner.invoke(app, ['train', '--model-class', 'cnnensemble'])
@@ -49,19 +49,12 @@ def test_train_options(data_dir, mocker):  # noqa: F811
 
     mocker.patch('zamba.cli.ModelManager.train', train_mock)
 
-    result = runner.invoke(app, ['train', '--train-data', str(data_dir)])
-    assert result.exit_code == 0
-
-    result = runner.invoke(app, ['train', '--model-path', str(assets_dir / 'new_model.h5')])
+    result = runner.invoke(app, ['train', '--train-data', str(data_dir), '--model-load-path', str(sample_model_path)])
     assert result.exit_code == 0
 
     result = runner.invoke(app, ['train', str(data_dir), '--download_region'])
     assert result.exit_code == 2
     assert 'no such option' in result.output
 
-    result = runner.invoke(app, ['train', '--framework', 'fastai'])
-    assert result.exit_code == 2
-    assert "invalid choice: fastai. (choose from keras, pytorch)" in result.output
-
-    result = runner.invoke(app, ['train', '--config', str(assets_dir / 'sample_config.yaml')])
+    result = runner.invoke(app, ['train', '--config', str(assets_dir / 'sample_train_config.yaml')])
     assert result.exit_code == 0
