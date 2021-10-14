33cfbff75dc83acc488505b8a3442568dbfc46dd
Jay Qi <jayqi@users.noreply.github.com>
2021-10-14 10:44:59 -0400

    Wrap upload_models in if __name__ = '__main__'

--- zamba/models/upload_models.py
+++ zamba/models/upload_models.py
@@ -5,11 +5,16 @@ from cloudpathlib import S3Path
 from zamba.models.config import MODEL_MAPPING
 
 
-for model in MODEL_MAPPING.keys():
-    for bucket in ["", "-eu", "-asia"]:
-        origin = S3Path(MODEL_MAPPING[model]["private_weights"])
-        public_file_name = MODEL_MAPPING[model]["public_weights"]
-        destination = S3Path(f"s3://drivendata-public-assets{bucket}/{public_file_name}")
+def upload_models():
+    for model in MODEL_MAPPING.keys():
+        for bucket in ["", "-eu", "-asia"]:
+            origin = S3Path(MODEL_MAPPING[model]["private_weights"])
+            public_file_name = MODEL_MAPPING[model]["public_weights"]
+            destination = S3Path(f"s3://drivendata-public-assets{bucket}/{public_file_name}")
 
-        logger.info(f"Uploading {origin} to {destination}")
-        origin.copy(destination, force_overwrite_to_cloud=True)
+            logger.info(f"Uploading {origin} to {destination}")
+            origin.copy(destination, force_overwrite_to_cloud=True)
+
+
+if __name__ == "__main__":
+    upload_models()
