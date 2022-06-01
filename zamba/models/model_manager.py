7986c417f33839c0a8d14ac66201472acbfb393a
Emily Miller <ejm714@gmail.com>
2022-05-31 18:34:58 -0700

    Use strategy instead of plugins for DDP (#187)
    * accelerator was deprecated
    
    * pin protobuf
    
    * replace plugin as well
    
    * remove unused import
    
    * add python extra for mkdocstrings
    
    * setup strategy properly
    
    * strategy has not been released yet
    
    * bonus typo fix

--- zamba/models/model_manager.py
+++ zamba/models/model_manager.py
@@ -268,8 +268,7 @@ def train_model(
         logger=tensorboard_logger,
         callbacks=callbacks,
         fast_dev_run=train_config.dry_run,
-        accelerator="ddp" if data_module.multiprocessing_context is not None else None,
-        plugins=DDPPlugin(find_unused_parameters=False)
+        strategy=DDPPlugin(find_unused_parameters=False)
         if data_module.multiprocessing_context is not None
         else None,
     )
