8e93ba7cd452b29b53a2f5c1b16796c67303dee9
ejm714 <emily@drivendata.org>
2022-05-31 17:44:03 -0700

    strategy has not been released yet

--- zamba/models/model_manager.py
+++ zamba/models/model_manager.py
@@ -12,7 +12,7 @@ import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.strategies import DDPStrategy
+from pytorch_lightning.plugins import DDPPlugin
 import torch
 
 from zamba import MODELS_DIRECTORY
@@ -268,7 +268,7 @@ def train_model(
         logger=tensorboard_logger,
         callbacks=callbacks,
         fast_dev_run=train_config.dry_run,
-        strategy=DDPStrategy(find_unused_parameters=False)
+        strategy=DDPPlugin(find_unused_parameters=False)
         if data_module.multiprocessing_context is not None
         else None,
     )
