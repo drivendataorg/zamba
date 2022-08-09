3f6bd13985c347e49c5490a9028ab2deb947cd12
AllenDowney <downey@allendowney.com>
2022-08-09 14:21:34 -0400

    First cut at get_cached_array_path

---
+++ tests/test_npy_cache.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+import yaml
+
+from cloudpathlib import S3Path
+
+from zamba.data.video import (
+    VideoLoaderConfig,
+    npy_cache,
+    get_cached_array_path,
+    load_video_frames,
+)
+
+config_yaml = """
+    cache_dir: local_data/cache
+    crop_bottom_pixels: 50
+    early_bias: false
+    ensure_total_frames: true
+    evenly_sample_total_frames: false
+    fps: 4.0
+    frame_indices: null
+    frame_selection_height: null
+    frame_selection_width: null
+    i_frames: false
+    megadetector_lite_config:
+        confidence: 0.25
+        fill_mode: score_sorted
+        image_height: 640
+        image_width: 640
+        n_frames: 16
+        nms_threshold: 0.45
+        seed: 55
+        sort_by_time: true
+    model_input_height: 240
+    model_input_width: 426
+    pix_fmt: rgb24
+    scene_threshold: null
+    total_frames: 16
+    cleanup_cache: false
+    cache_dir: data/cache
+"""
+
+
+def test_get_cached_array_path():
+    config_dict = yaml.safe_load(config_yaml)
+    config = VideoLoaderConfig(**config_dict)
+
+    # NOTE: the validation in VideoLoaderConfig changes some fields, so this fails
+    # assert dict(config) == config_dict
+
+    cached_load_video_frames = npy_cache(
+        cache_path=config.cache_dir, cleanup=config.cleanup_cache
+    )(load_video_frames)
+    assert isinstance(cached_load_video_frames, type(load_video_frames))
+
+    vid_path_str = "s3://drivendata-client-zamba/data/raw/noemie/Taï_cam197_683044_652175_20161223/01090065.AVI"
+    vid_path_s3 = S3Path(vid_path_str)
+    vid_path_path = Path(vid_path_s3.key)
+
+    expected = Path(
+        "data/cache/4c54037cf092a26598988b31c84d7999d187a123/data/raw/noemie/Taï_cam197_683044_652175_20161223/01090065.npy"
+    )
+
+    # pass the path as a string
+    path = get_cached_array_path(config, vid_path_str, config.cache_dir)
+    assert path == expected
+
+    # pass the path as S3Path
+    path = get_cached_array_path(config, vid_path_s3, config.cache_dir)
+    assert path == expected
+
+    # pass the path as a Path
+    path = get_cached_array_path(config, vid_path_path, config.cache_dir)
+    assert path == expected
+
+    # pass the config as a dictionary
+    config_dict = dict(config)
+    path = get_cached_array_path(config_dict, vid_path_str, config.cache_dir)
+    assert path == expected
+
+    # changing config.cache_dir and/or config.cleanup_cache should not affect the key
+    config_dict["cache_dir"] = "something/else"
+    config_dict["cleanup_cache"] = "not even bool"
+    path = get_cached_array_path(config_dict, vid_path_path, config.cache_dir)
+    assert path == expected
+
+    # changing anything else should
+    config_dict["total_frames"] = 8
+    path = get_cached_array_path(config_dict, vid_path_path, config.cache_dir)
+    expected2 = Path(
+        "data/cache/947ddec6d01cd16dd36afa9f5a24030b2d494463/data/raw/noemie/Taï_cam197_683044_652175_20161223/01090065.npy"
+    )
+    assert path == expected2
--- zamba/data/video.py
+++ zamba/data/video.py
@@ -325,6 +325,42 @@ class VideoLoaderConfig(BaseModel):
         return values
 
 
+def get_cached_array_path(config, vid_path, cache_dir):
+    """Get the path to where the cached array would be, if it exists.
+
+    config: dictionary or VideoLoaderConfig
+    vid_path: string path to the video, or Path, or S3Path
+    cache_dir: string directory name, or Path, or S3Path
+
+    returns: Path object to the cached data
+    """
+    if isinstance(config, VideoLoaderConfig):
+        config = dict(config)
+
+    # don't include `cleanup_cache` or `cache_dir` in the hashed config
+    # NOTE: sorting the keys avoids a cache miss if we see the same config in a different order
+    keys = config.keys() - {"cleanup_cache", "cache_dir"}
+    hashed_part = {k: config[k] for k in sorted(keys)}
+
+    # hash config for inclusion in filename
+    hash_str = hashlib.sha1(str(hashed_part).encode("utf-8")).hexdigest()
+    logger.opt(lazy=True).debug(
+        "Generated hash {hash_str} from {hashed_part}",
+        hash_str=lambda: hash_str,
+        config=lambda: str(config),
+    )
+
+    # strip leading "/" in absolute path
+    vid_path = AnyPath(str(vid_path).lstrip("/"))
+
+    # if the video is in S3, drop the prefix and bucket name
+    if isinstance(vid_path, S3Path):
+        vid_path = AnyPath(vid_path.key)
+
+    npy_path = AnyPath(cache_dir) / hash_str / vid_path.with_suffix(".npy")
+    return npy_path
+
+
 class npy_cache:
     def __init__(self, cache_path: Optional[Path] = None, cleanup: bool = False):
         self.cache_path = cache_path
@@ -341,24 +377,8 @@ class npy_cache:
             except Exception:
                 config = kwargs
 
-            # don't include cleanup in the hashed config
-            config.pop("cleanup_cache")
+            npy_path = get_cached_array_path(config, vid_path, self.cache_path)
 
-            # hash config for inclusion in filename
-            hash_str = hashlib.sha1(str(config).encode("utf-8")).hexdigest()
-            logger.opt(lazy=True).debug(
-                "Generated hash {hash_str} from {config}",
-                hash_str=lambda: hash_str,
-                config=lambda: str(config),
-            )
-
-            # strip leading "/" in absolute path
-            vid_path = AnyPath(str(vid_path).lstrip("/"))
-
-            if isinstance(vid_path, S3Path):
-                vid_path = AnyPath(vid_path.key)
-
-            npy_path = self.cache_path / hash_str / vid_path.with_suffix(".npy")
             # make parent directories since we're using absolute paths
             npy_path.parent.mkdir(parents=True, exist_ok=True)
 
