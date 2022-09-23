b0b2bfe9fcbe332cb2400f0691ce39e6e949d92f
Emily Miller <ejm714@gmail.com>
2022-09-23 15:06:09 -0700

    remove unused params; set default labels explciitly to True for one test since the default is now none

--- tests/test_instantiate_model.py
+++ tests/test_instantiate_model.py
@@ -8,17 +8,15 @@ from zamba.models.model_manager import instantiate_model
 from conftest import DummyZambaVideoClassificationLightningModule
 
 
-def test_scheduler_ignored_for_prediction(dummy_checkpoint, tmp_path):
+def test_scheduler_ignored_for_prediction(dummy_checkpoint):
     """Tests whether we can instantiate a model for prediction and ignore scheduler config."""
     original_hyperparams = torch.load(dummy_checkpoint)["hyper_parameters"]
     assert original_hyperparams["scheduler"] is None
 
     model = instantiate_model(
         checkpoint=dummy_checkpoint,
-        weight_download_region="us",
         scheduler_config=SchedulerConfig(scheduler="StepLR", scheduler_params=None),
         labels=None,
-        model_cache_dir=tmp_path,
     )
     # since labels is None, we are predicting. as a result, hparams are not updated
     assert model.hparams["scheduler"] is None
@@ -26,14 +24,12 @@ def test_scheduler_ignored_for_prediction(dummy_checkpoint, tmp_path):
     # # in Train Config, which contains ModelParams, labels cannot be None
 
 
-def test_default_scheduler_used(time_distributed_checkpoint, tmp_path):
+def test_default_scheduler_used(time_distributed_checkpoint):
     """Tests instantiate model uses the default scheduler from the hparams on the model."""
     default_scheduler_passed_model = instantiate_model(
         checkpoint=time_distributed_checkpoint,
-        weight_download_region="us",
         scheduler_config="default",
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
     )
 
     # with "default" scheduler_config, hparams from training are used
@@ -43,14 +39,12 @@ def test_default_scheduler_used(time_distributed_checkpoint, tmp_path):
     )
 
 
-def test_scheduler_used_if_passed(time_distributed_checkpoint, tmp_path):
+def test_scheduler_used_if_passed(time_distributed_checkpoint):
     """Tests that scheduler config gets used and overrides scheduler on time distributed training."""
     scheduler_passed_model = instantiate_model(
         checkpoint=time_distributed_checkpoint,
-        weight_download_region="us",
         scheduler_config=SchedulerConfig(scheduler="StepLR"),
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
     )
 
     # hparams reflect user specified scheduler config
@@ -61,28 +55,24 @@ def test_scheduler_used_if_passed(time_distributed_checkpoint, tmp_path):
     # check scheduler params get used
     scheduler_params_passed_model = instantiate_model(
         checkpoint=time_distributed_checkpoint,
-        weight_download_region="us",
         scheduler_config=SchedulerConfig(scheduler="StepLR", scheduler_params={"gamma": 0.3}),
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
     )
     assert scheduler_params_passed_model.hparams["scheduler_params"] == {"gamma": 0.3}
 
 
-def test_remove_scheduler(time_distributed_checkpoint, tmp_path):
+def test_remove_scheduler(time_distributed_checkpoint):
     """Tests that a scheduler config with None values removes the scheduler on the model."""
     remove_scheduler_model = instantiate_model(
         checkpoint=time_distributed_checkpoint,
-        weight_download_region="us",
         scheduler_config=SchedulerConfig(scheduler=None),
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
     )
     # pretrained model has scheduler but this is overridden with SchedulerConfig
     assert remove_scheduler_model.hparams["scheduler"] is None
 
 
-def test_head_not_replaced_for_species_subset(dummy_trained_model_checkpoint, tmp_path):
+def test_head_not_replaced_for_species_subset(dummy_trained_model_checkpoint):
     """Tests that training a model using labels that are a subset of the model species resumes
     model training without replacing the model head."""
     original_model = DummyZambaVideoClassificationLightningModule.from_disk(
@@ -91,10 +81,9 @@ def test_head_not_replaced_for_species_subset(dummy_trained_model_checkpoint, tm
 
     model = instantiate_model(
         checkpoint=dummy_trained_model_checkpoint,
-        weight_download_region="us",
         scheduler_config="default",
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
+        use_default_model_labels=True,
     )
 
     assert (model.head.weight == original_model.head.weight).all()
@@ -106,7 +95,7 @@ def test_head_not_replaced_for_species_subset(dummy_trained_model_checkpoint, tm
     assert model.model[-1].out_features == 3
 
 
-def test_not_use_default_model_labels(dummy_trained_model_checkpoint, tmp_path):
+def test_not_use_default_model_labels(dummy_trained_model_checkpoint):
     """Tests that training a model using labels that are a subset of the model species but
     with use_default_model_labels=False replaces the model head."""
     original_model = DummyZambaVideoClassificationLightningModule.from_disk(
@@ -115,10 +104,8 @@ def test_not_use_default_model_labels(dummy_trained_model_checkpoint, tmp_path):
 
     model = instantiate_model(
         checkpoint=dummy_trained_model_checkpoint,
-        weight_download_region="us",
         scheduler_config="default",
         labels=pd.DataFrame([{"filepath": "gorilla.mp4", "species_gorilla": 1}]),
-        model_cache_dir=tmp_path,
         use_default_model_labels=False,
     )
 
@@ -129,7 +116,7 @@ def test_not_use_default_model_labels(dummy_trained_model_checkpoint, tmp_path):
     assert model.model[-1].out_features == 1
 
 
-def test_head_replaced_for_new_species(dummy_trained_model_checkpoint, tmp_path):
+def test_head_replaced_for_new_species(dummy_trained_model_checkpoint):
     """Tests that training a model using labels that are a not subset of the model species
     finetunes the model and replaces the model head."""
     original_model = DummyZambaVideoClassificationLightningModule.from_disk(
@@ -138,10 +125,8 @@ def test_head_replaced_for_new_species(dummy_trained_model_checkpoint, tmp_path)
 
     model = instantiate_model(
         checkpoint=dummy_trained_model_checkpoint,
-        weight_download_region="us",
         scheduler_config="default",
         labels=pd.DataFrame([{"filepath": "alien.mp4", "species_alien": 1}]),
-        model_cache_dir=tmp_path,
     )
 
     assert (model.head.weight != original_model.head.weight).all()
@@ -159,10 +144,8 @@ def test_finetune_new_labels(labels_absolute_path, model, tmp_path):
     )
     model = instantiate_model(
         checkpoint=config.checkpoint,
-        weight_download_region=config.weight_download_region,
         scheduler_config="default",
         labels=pd.DataFrame([{"filepath": "kangaroo.mp4", "species_kangaroo": 1}]),
-        model_cache_dir=tmp_path,
     )
     assert model.species == ["kangaroo"]
 
@@ -178,11 +161,9 @@ def test_resume_subset_labels(labels_absolute_path, model, tmp_path):
     )
     model = instantiate_model(
         checkpoint=config.checkpoint,
-        weight_download_region=config.weight_download_region,
         scheduler_config=SchedulerConfig(scheduler="StepLR", scheduler_params=None),
         # pick species that is present in all models
         labels=pd.DataFrame([{"filepath": "bird.mp4", "species_bird": 1}]),
-        model_cache_dir=tmp_path,
     )
     assert model.hparams["scheduler"] == "StepLR"
 
