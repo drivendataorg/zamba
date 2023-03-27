ba747c7e8a1a1be08fb5ed04363fc4b2f58890fb
ejm714 <emily@drivendata.org>
2023-03-27 14:24:01 -0700

    set gt 1

--- zamba/models/model_manager.py
+++ zamba/models/model_manager.py
@@ -289,7 +289,7 @@ def train_model(
         callbacks=callbacks,
         fast_dev_run=train_config.dry_run,
         strategy=DDPStrategy(find_unused_parameters=False)
-        if (data_module.multiprocessing_context is not None) and (train_config.gpus > 0)
+        if (data_module.multiprocessing_context is not None) and (train_config.gpus > 1)
         else "auto",
     )
 
