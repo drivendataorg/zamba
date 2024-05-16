ef81a1a13bb5b129ab6a04aa972fc57f44903687
ejm714 <emily@drivendata.org>
2024-05-15 17:40:06 -0700

    format

--- tests/test_load_video_frames.py
+++ tests/test_load_video_frames.py
@@ -458,7 +458,9 @@ def test_megadetector_lite_yolox_dog(tmp_path):
             "-y",
         ]
     )
-    frames = load_video_frames(tmp_path / "dog.mp4", megadetector_lite_config=MegadetectorLiteYoloXConfig())
+    frames = load_video_frames(
+        tmp_path / "dog.mp4", megadetector_lite_config=MegadetectorLiteYoloXConfig()
+    )
 
     # Check that we detected the correct number of frames
     assert len(frames) == len(object_frame_indices)
