3a46bde4af022dc3ee9953cd429efacdeda8e1f5
Peter Bull <peter@drivendata.org>
2025-03-13 14:06:10 -0700

    densepose weights

--- zamba/utils_cli.py
+++ zamba/utils_cli.py
@@ -9,6 +9,7 @@ import yaml
 from zamba import MODELS_DIRECTORY
 from zamba.images.config import ImageModelEnum
 from zamba.models.config import ModelEnum
+from zamba.models.densepose.densepose_manager import MODELS as DENSEPOSE_MODELS
 from zamba.models.utils import download_weights
 from zamba.settings import get_model_cache_dir
 
@@ -30,20 +31,30 @@ def dl_weights(
     ),
 ):
     cache_dir = cache_dir or get_model_cache_dir()
+
+    weights_filenames = []
+
+    # get weights filenames for model files
     model_names = list(chain(ModelEnum.__members__.keys(), ImageModelEnum.__members__.values()))
-    for model_name in tqdm(model_names, desc="Downloading weights for all models..."):
+    for model_name in model_names:
         with (MODELS_DIRECTORY / model_name / "config.yaml").open() as f:
             config_dict = yaml.safe_load(f)
 
-        cache_path = cache_dir / config_dict["public_checkpoint"]
+        weights_filenames.append(config_dict["public_checkpoint"])
+
+    # get weights filenames for densepose models
+    for model_name, model_config in DENSEPOSE_MODELS.items():
+        weights_filenames.append(model_config["weights"])
+
+    # download weights
+    for weights_filename in tqdm(weights_filenames, desc="Downloading weights for all models..."):
+        cache_path = cache_dir / weights_filename
 
         if not overwrite and cache_path.exists():
             logger.info(
-                f"Weights for {model_name} already exist in {cache_path}, skipping. Use --overwrite to download again."
+                f"Weights {weights_filename} already exist in {cache_path}, skipping. Use --overwrite to download again."
             )
             continue
 
-        logger.info(
-            f"Downloading weights for {model_name} from {config_dict['public_checkpoint']} to {cache_path}"
-        )
-        download_weights(config_dict["public_checkpoint"], cache_path)
+        logger.info(f"Downloading weights {weights_filename} to {cache_path}")
+        download_weights(weights_filename, cache_path)
